7c879a512c57a10009a65176d99f458ed50e00b5
wtanaka.com <wtanaka@yahoo.com>
2017-04-01 18:21:57 -1000

    Remove unnecessary explicit constructor

--- beam/src/test/java/com/wtanaka/beam/StreamIOTest.java
+++ beam/src/test/java/com/wtanaka/beam/StreamIOTest.java
@@ -1,5 +1,21 @@
 /*
- * Copyright (C) 2017 Wesley Tanaka <http://wtanaka.com>
+ * com.wtanaka.beam
+ *
+ * Copyright (C) 2017 Wesley Tanaka <http://wtanaka.com/>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.wtanaka.beam;
 
@@ -30,8 +46,7 @@ public class StreamIOTest
          {0x68, 0x0a, 0x65, 0x0a, 0x6c, 0x0a, 0x6c, 0x0a, 0x6f, 0x0a});
       PCollection<String> lines = pipeline.apply(
          new StreamIO.Read.Bound(bais));
-      PAssert.that(lines).containsInAnyOrder(new String[]{"h", "e", "l",
-         "l", "o"});
+      PAssert.that(lines).containsInAnyOrder("h", "e", "l", "l", "o");
    }
 
    @Test
@@ -40,7 +55,7 @@ public class StreamIOTest
       final Pipeline pipeline = TestPipeline.create();
       final ByteArrayOutputStream baos = new ByteArrayOutputStream();
       final PCollection<String> source = pipeline.apply(
-         Create.<String>of("foo", "bar", "baz"));
+         Create.of("foo", "bar", "baz"));
       source.apply(new StreamIO.Write.Bound(baos));
    }
 }
