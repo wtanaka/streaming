0e279bac900fac3074948948151435a891cc1240
wtanaka.com <wtanaka@yahoo.com>
2017-04-01 18:21:35 -1000

    Remove unnecessary cast

--- beam/src/main/java/com/wtanaka/beam/StreamIO.java
+++ beam/src/main/java/com/wtanaka/beam/StreamIO.java
@@ -1,5 +1,21 @@
 /*
- * Copyright (C) 2017 Wesley Tanaka <http://wtanaka.com>
+ * com.wtanaka.beam
+ *
+ * Copyright (C) 2017 Wesley Tanaka <http://wtanaka.com/>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.wtanaka.beam;
 
@@ -121,8 +137,7 @@ public class StreamIO
             final StreamLineIterable stream = new StreamLineIterable(
                m_inputStream);
             final PTransform<PBegin, PCollection<String>> transform =
-               Create.<String>of((Iterable<String>) stream)
-                  .withCoder(DEFAULT_TEXT_CODER);
+               Create.of(stream).withCoder(DEFAULT_TEXT_CODER);
             return input.apply(transform);
          }
       }
