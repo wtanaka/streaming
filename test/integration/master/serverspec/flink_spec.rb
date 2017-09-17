f9467e94517645293957b60b3b016d71a684a545
wtanaka.com <wtanaka@users.noreply.github.com>
2017-09-16 14:38:15 -1000

    Flink 1.3.2

--- test/integration/master/serverspec/flink_spec.rb
+++ test/integration/master/serverspec/flink_spec.rb
@@ -6,4 +6,20 @@ describe "Flink Master" do
   describe port(8081) do
     it { should be_listening.with('tcp') }
   end
+
+  describe port(6123) do
+    it { should be_listening.with('tcp') }
+  end
+end
+
+describe "Zookeeper" do
+  describe port(2181) do
+    it { should be_listening.with('tcp') }
+  end
+end
+
+describe "Kafka" do
+  describe port(9092) do
+    it { should be_listening.with('tcp') }
+  end
 end
